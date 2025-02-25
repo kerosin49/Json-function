0cd74a43521e8a5a0af554592fb15cafdb67705a
Sanya <kerosin49@gmail.com>
2025-02-25 22:58:17 +0300

    xyeta neponyatnaya

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+# import json
+
+# def get_student_by_id(filename, student_id):
+#     with open(filename, 'r') as f:
+#         students = json.load(f)
+#         for student in students:
+#             if student.get('student_id') == student_id:
+#                 return student
+#     return None
+
+# student = get_student_by_id('data.json', 3)
+
+# if student:
+#     print(student)
+# else:
+#     print("Студент не найден")
 import json
-
-
-with open('data.json') as f:
-    data = json.load(f)
-    print(f)
+f = open('data.json', 'r')
+data = json.load(f)
+f.close()
+print(data)
